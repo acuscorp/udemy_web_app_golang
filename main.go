ac5a92e2a4e6331904c288247bee0626644eeb3c
noe.acuna <noe.acuna@solera.com>
2022-09-30 11:12:43 -0500

    init serving HTKML Templates

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 )
 
@@ -10,32 +9,11 @@ const PORT_NUMBER = ":8080"
 
 // this is the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the Home page!")
+
 }
 
 // this is the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	x := 5
-	y := 2
-	sum, _ := AddValues(x, y)
-	fmt.Fprintf(w, "This is the about page and the sum of %d and %d is %d", x, y, sum)
-
-}
-
-func AddValues(x, y int) (int, error) {
-	return x + y, nil
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	x := float32(1000)
-	y := float32(0)
-	f, err := divideValues(x, y)
-	if err != nil {
-		fmt.Fprintf(w, "%f cannot devide by 0", x)
-	} else {
-
-		fmt.Fprintf(w, "%f divided by %f is %f", x, y, f)
-	}
 
 }
 
