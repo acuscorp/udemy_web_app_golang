f590474c2b064684b0b3f5e5876e939cdbad57f0
noe.acuna <noe.acuna@solera.com>
2022-10-03 09:17:55 -0500

    building a ismple template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,10 +3,11 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	// this parse files, and can receive one or more files, in this case the content and de base
 	parseTemplate, _ := template.ParseFiles(templatesFolderPath()+tmpl, templatesFolderPath()+"base.layout.tmpl")
 	fmt.Printf("tmpl: %v\n", tmpl)
@@ -17,6 +18,29 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 }
 
 var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+
+	var err error
+
+	_, inMap := tc[t]
+
+	if !inMap {
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		log.Println("using cached template")
+	}
+
+	err = tc[t].Execute(w, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func createTemplateCache(t string) error {
 	templates := []string{
@@ -29,6 +53,7 @@ func createTemplateCache(t string) error {
 		return err
 	}
 	tc[t] = tmpl
+	return nil
 }
 
 func templatesFolderPath() string {
