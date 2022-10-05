fd5a9cb4bf2e6956856c32daccab8723728e6265
noe.acuna <noe.acuna@solera.com>
2022-10-05 10:59:27 -0500

    Usign chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/acuscorp/go-course/pkg/config"
 	"github.com/acuscorp/go-course/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
